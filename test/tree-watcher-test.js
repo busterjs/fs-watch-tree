791668e3cc0a86d08fd9f111ce82fac5e49bdcc5
Magnar Sveen <magnars@gmail.com>
2012-04-26 08:16:08 +0200

    Run tests for both osx and unix.

--- test/tree-watcher-test.js
+++ test/tree-watcher-test.js
@@ -35,92 +35,96 @@ function createTree(callback) {
     });
 }
 
-buster.testCase('tree-watcher', {
-    setUp: function (done) {
-        this.timeout = 1000;
-        createTree(function () {
-            this.os = osWatch.on(this, "unix");
-            this.watcher = treeWatcher.create(helper.ROOT, ["ignored"]);
-            this.watcher.init().then(done);
-        }.bind(this));
-    },
+function testPlatform(platform) {
+    buster.testCase('tree-watcher-' + platform, {
+        setUp: function (done) {
+            this.timeout = 1000;
+            createTree(function () {
+                this.os = osWatch.on(this, platform);
+                this.watcher = treeWatcher.create(helper.ROOT, ["ignored"]);
+                this.watcher.init().then(done);
+            }.bind(this));
+        },
 
-    tearDown: function (done) {
-        rmrf(helper.ROOT, done);
-    },
+        tearDown: function (done) {
+            rmrf(helper.ROOT, done);
+        },
 
-    "end closes all the watches": function () {
-        this.watcher.end();
-        assert.equals(this.os.watchers.length, 0);
-    },
+        "end closes all the watches": function () {
+            this.watcher.end();
+            assert.equals(this.os.watchers.length, 0);
+        },
 
-    "emits 'file:change'": eventTest({
-        event: "file:change",
-        action: function () {
-            return this.os.change(p("exists.txt"));
-        }
-    }),
+        "emits 'file:change'": eventTest({
+            event: "file:change",
+            action: function () {
+                return this.os.change(p("exists.txt"));
+            }
+        }),
 
-    "emits 'file:change' for nested files": eventTest({
-        event: "file:change",
-        action: function () {
-            return this.os.change(p("subdir", "nested.txt"));
-        }
-    }),
+        "emits 'file:change' for nested files": eventTest({
+            event: "file:change",
+            action: function () {
+                return this.os.change(p("subdir", "nested.txt"));
+            }
+        }),
 
-    "emits 'file:create'": eventTest({
-        event: "file:create",
-        action: function () {
-            return this.os.create(p("spanking-new.txt"));
-        }
-    }),
+        "emits 'file:create'": eventTest({
+            event: "file:create",
+            action: function () {
+                return this.os.create(p("spanking-new.txt"));
+            }
+        }),
 
-    "emits 'file:delete'": eventTest({
-        event: "file:delete",
-        action: function () {
-            return this.os.rm(p("exists.txt"));
-        }
-    }),
+        "emits 'file:delete'": eventTest({
+            event: "file:delete",
+            action: function () {
+                return this.os.rm(p("exists.txt"));
+            }
+        }),
 
-    "emits 'dir:create'": eventTest({
-        event: "dir:create",
-        action: function () {
-            return this.os.mkdir(p("newone"));
-        }
-    }),
+        "emits 'dir:create'": eventTest({
+            event: "dir:create",
+            action: function () {
+                return this.os.mkdir(p("newone"));
+            }
+        }),
 
-    "emits 'dir:delete'": eventTest({
-        event: "dir:delete",
-        action: function () {
-            return this.os.rmdir(p("deleteme"));
-        }
-    }),
+        "emits 'dir:delete'": eventTest({
+            event: "dir:delete",
+            action: function () {
+                return this.os.rmdir(p("deleteme"));
+            }
+        }),
 
-    "ignores files": function (done) {
-        var spy = this.spy();
-        this.watcher.on("file:change", spy);
-
-        this.os.change(p("subdir", "ignored.txt")).then(done(function () {
-            refute.called(spy);
-        }));
-    },
-
-    "ignores directories": function (done) {
-        var spy = this.spy();
-        this.watcher.on("file:create", spy);
-
-        this.os.create(p("ignored", "file.txt")).then(done(function () {
-            refute.called(spy);
-        }));
-    },
-
-    "watches new files": function (done) {
-        this.os.create(p("newfile.txt")).then(function () {
+        "ignores files": function (done) {
             var spy = this.spy();
             this.watcher.on("file:change", spy);
-            this.os.change(p("newfile.txt")).then(done(function () {
-                assert.calledOnce(spy);
+
+            this.os.change(p("subdir", "ignored.txt")).then(done(function () {
+                refute.called(spy);
             }));
-        }.bind(this));
-    }
-});
+        },
+
+        "ignores directories": function (done) {
+            var spy = this.spy();
+            this.watcher.on("file:create", spy);
+
+            this.os.create(p("ignored", "file.txt")).then(done(function () {
+                refute.called(spy);
+            }));
+        },
+
+        "watches new files": function (done) {
+            this.os.create(p("newfile.txt")).then(function () {
+                var spy = this.spy();
+                this.watcher.on("file:change", spy);
+                this.os.change(p("newfile.txt")).then(done(function () {
+                    assert.calledOnce(spy);
+                }));
+            }.bind(this));
+        }
+    });
+}
+
+["unix", "osx"].forEach(testPlatform);
